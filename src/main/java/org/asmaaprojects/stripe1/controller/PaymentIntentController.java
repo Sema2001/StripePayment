f05f41c3bdc72eb31d55cd354ba5e936412d2b57
asmaa <asmaabenhaili1@gmail.com>
2024-12-17 22:53:11 +0100

    first commit

---
+++ src/main/java/org/asmaaprojects/stripe1/controller/PaymentIntentController.java
@@ -0,0 +1,39 @@
+package org.asmaaprojects.stripe1.controller;
+
+
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+import org.asmaaprojects.stripe1.model.Request;
+import org.asmaaprojects.stripe1.model.Response;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentIntentController {
+    @PostMapping("/create-payment-intent")
+    public Response createPaymentIntent(@RequestBody Request request)
+            throws StripeException {
+        PaymentIntentCreateParams params =
+                PaymentIntentCreateParams.builder()
+                        .setAmount(request.getAmount() * 100L)
+                        .putMetadata("productName",
+                                request.getProductName())
+                        .setCurrency("usd")
+                        .setAutomaticPaymentMethods(
+                                PaymentIntentCreateParams
+                                        .AutomaticPaymentMethods
+                                        .builder()
+                                        .setEnabled(true)
+                                        .build()
+                        )
+                        .build();
+
+        PaymentIntent intent =
+                PaymentIntent.create(params);
+
+        return new Response(intent.getId(),
+                intent.getClientSecret());
+    }
+}
---
+++ src/test/java/org/asmaaprojects/stripe1/Stripe1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.asmaaprojects.stripe1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stripe1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
